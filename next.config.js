308bd78ebc6a0f7bf6f9188c42306936b3d87e2a
Nadim Chowdhury <40499378+nadim-chowdhury@users.noreply.github.com>
2023-04-28 00:13:41 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  
-  experiments: {
-    topLevelAwait: true,
+
+  webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    return config;
   },
 
   images: {
